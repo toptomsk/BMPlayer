f54ac2295739449094af3ae5ace0b65d03c554e6
toptomsk <31775272+toptomsk@users.noreply.github.com>
2020-05-06 09:07:52 +0700

    Update BMPlayer.swift

--- Source/BMPlayer.swift
+++ Source/BMPlayer.swift
@@ -82,9 +82,10 @@ open class BMPlayer: UIView {
     fileprivate var isFullScreen:Bool {
         get {
             switch UIDevice.current.orientation {
-            case .landscapeLeft, .landscapeRight: return true
-            case .portrait, .portraitUpsideDown: return false
-            default: return false
+                case .landscapeLeft, .landscapeRight: return true
+                case .portrait, .portraitUpsideDown: return false
+                default: return false
+            }
         }
     }
     
